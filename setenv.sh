31d37da067e2587a835b743f86743c80945fc93d
sgwgsw <43614862+sgwgsw@users.noreply.github.com>
2022-03-19 22:39:58 -0700

    Update setenv.sh

--- setenv.sh
+++ setenv.sh
@@ -1,4 +1,4 @@
 #The environment variable CLASSPATH is unset in catalina.sh/catalina.bat
-CLASSPATH=/opt/tomcat/lib/log4j-api-2.14.0.jar:/opt/tomcat/lib/log4j-core-2.14.0.jar:/opt/tomcat/lib/log4j-jul-2.14.0.jar
-JAVA_OPTS=-Dlog4j.configurationFile=/opt/tomcat/conf/log4j2.xml
+CLASSPATH=/usr/local/tomcat/lib/log4j-api-2.14.0.jar:/usr/local/tomcat/lib/log4j-core-2.14.0.jar:/usr/local/tomcat/lib/log4j-jul-2.14.0.jar
+#JAVA_OPTS=-Dlog4j.configurationFile=/opt/tomcat/conf/log4j2.xml
 LOGGING_MANAGER=-Djava.util.logging.manager=org.apache.logging.log4j.jul.LogManager
